e5edd535360ebef4f3aef0ed6c86664676ca60d8
paska <pavel.javornik00@gmail.com>
2026-01-03 11:09:15 -0500

    feat: finished chapter 20

--- chapter_20_final_project/hello/src/main.rs
+++ chapter_20_final_project/hello/src/main.rs
@@ -2,15 +2,18 @@ use std::{
     fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}
 };
 
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+    // for stream in listener.incoming().take(5) { // only allow 5 requests
+    for stream in listener.incoming(){
         let stream = stream.unwrap();
-        println!("Connection established!");
-        handle_connection(stream);
+        //println!("Connection established!");
+        pool.execute(|| { handle_connection(stream); });
     }
+    println!("Shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -20,12 +23,18 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty()) // two newlines signal end of request
         .collect();
-    println!("Request: {:#?}", http_request);
+    //println!("Request: {:#?}", http_request);
 
     //let response = "HTTP/1.1 200 OK\r\n\r\n"; // Standard response
-
-    let status = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let request_line = http_request[0].as_str();
+    let (status, file_name) = 
+        if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
     let response = format!(
         "{status}\r\n\
