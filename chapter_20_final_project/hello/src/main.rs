914080017b9fe52e957bd770211b7a24396a284c
paska <pavel.javornik00@gmail.com>
2026-01-02 22:39:55 -0500

    feat: starting chapter 20

---
+++ chapter_20_final_project/hello/src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}
+};
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty()) // two newlines signal end of request
+        .collect();
+    println!("Request: {:#?}", http_request);
+
+    //let response = "HTTP/1.1 200 OK\r\n\r\n"; // Standard response
+
+    let status = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+}
